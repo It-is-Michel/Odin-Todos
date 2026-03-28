7f44efe6cb4c5403803cadd3089a5107c645e987
Michel <221404137+It-is-Michel@users.noreply.github.com>
2026-03-28 12:03:16 -0300

    feat(TodoComp): Set 8 properties in the constructor

--- src/todo-component/todo.js
+++ src/todo-component/todo.js
@@ -23,6 +23,17 @@ class Todo {
   #dueDate;
   /** @type {array} */
   #dueDateAlerts;
+
+  constructor(todoData) {
+    this.title = todoData.title;
+    this.desc = todoData.desc;
+    this.subTasks = todoData.subTasks;
+    this.note = todoData.note;
+    this.successCriteria = todoData.successCriteria;
+    this.priority = todoData.priority;
+    this.dueDate = todoData.dueDate;
+    this.dueDateAlerts = todoData.dueDateAlerts;
+  }
 };
 
 export default Todo;
