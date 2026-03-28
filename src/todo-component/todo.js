e714a2258c2551d83d9911d62984ce8fe37ad351
Michel <221404137+It-is-Michel@users.noreply.github.com>
2026-03-28 11:59:15 -0300

    feat(TodoComp): Add 8 properties

--- src/todo-component/todo.js
+++ src/todo-component/todo.js
@@ -7,7 +7,22 @@ class TodoAlertService {
 };
 
 class Todo {
-  
+  /** @type {string} */
+  #title;
+  /** @type {string} */
+  #desc;
+  /** @type {array} */
+  #subTasks;
+  /** @type {string} */
+  #note;
+  /** @type {string} */
+  #successCriteria;
+  /** @type {number} */
+  #priority;
+  /** @type {Date} */
+  #dueDate;
+  /** @type {array} */
+  #dueDateAlerts;
 };
 
 export default Todo;
